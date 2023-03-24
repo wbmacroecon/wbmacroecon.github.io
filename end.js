28be38fd7bfaca01ecd61ec78f7981f2c3542188
great elf <evilelflmao@gmail.com>
2023-03-24 16:09:34 -0400

    endscreen

--- end.js
+++ end.js
@@ -1,4 +1,4 @@
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const finalScore = document.getElementById("finalScore");
 const maxQuestions = localStorage.getItem("maxQuestions");
-finalScore.innerText = `${mostRecentScore} or ${(mostRecentScore/maxQuestions)*100}%`;
+finalScore.innerText = `${mostRecentScore} or ${math.Round(mostRecentScore/maxQuestions)*100}%`;
