2c182d29b5208c37039cbd8bf9c7264940f8a056
great elf <evilelflmao@gmail.com>
2023-04-20 19:57:45 -0400

    resources

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ fetch("questions.json").then( res => {
 
 //CONSTANTS
 const CORRECT_BONUS = 1;
-const MAX_QUESTIONS = 6;
+const MAX_QUESTIONS = 12;
 localStorage.setItem("maxQuestions", MAX_QUESTIONS);
 
 startGame = () => {
